c6f4c713bba07248173900807fcbf284d7508ed4
Aashu Rajbhar <aashurajbhar20@gmail.com>
2024-06-09 23:35:18 +0545

    add game logic with javascript

---
+++ app.js
@@ -0,0 +1,95 @@
+let gameseq=[];
+let userseq=[];
+
+score=0;
+let started=false;
+let level=0;
+
+let h2=document.querySelector("h2");
+let btns=["red","green","yellow","purple"]
+
+
+
+document.addEventListener("keypress",function(){
+    if(started==false){
+        console.log("game is started")
+        started=true;
+    }
+    levelup();
+})
+
+
+function gameflash(btn){
+  
+    btn.classList.add("gameflash");
+    setTimeout(function(){
+        // btn.classList.toggle("flash");
+        btn.classList.remove('gameflash'); 
+    },300)
+}
+
+function userflash(btn){
+  
+    btn.classList.add("userflash");
+    setTimeout(function(){
+        // btn.classList.toggle("flash");
+        btn.classList.remove('userflash'); 
+    },300)
+}
+
+function levelup(){
+    userseq=[];
+   score++
+    level ++;
+    h2.innerText=`Level: ${level}`;
+    let ranindex=Math.floor(Math.random()*4);
+    let rancolor=btns[ranindex];
+    let ranbtn=document.querySelector(`.${rancolor}`)
+    gameflash(ranbtn);
+    gameseq.push(rancolor);
+    console.log(gameseq)
+}
+
+function check(indx){
+  console.log(`current level :${indx}`)
+  if(gameseq[indx]==userseq[indx]){
+    if(gameseq.length==userseq.length){
+        setTimeout(levelup,1000)
+    }
+    console.log(`you pass level ${level}`)
+  }
+  else{
+    gameover();
+    h2.innerText=`Game over! press any key to start the game`
+    document.querySelector("body").style.backgroundColor="red";
+    setTimeout(function(){
+        document.querySelector("body").style.backgroundColor="white";
+
+    },250)
+    
+  }
+}
+
+function btnpress(){
+    console.log(this);
+    btn=this;
+    let usercolor=btn.getAttribute("id");
+    userseq.push(usercolor);
+    userflash(btn);
+    check(userseq.length-1);
+}
+
+
+let allbtn=document.querySelectorAll(".btn")
+for(btn of allbtn){
+    btn.addEventListener("click",btnpress)
+}
+
+function gameover(){
+    started=false;
+    level=0;
+    gameseq=[];
+    userseq=[];
+
+    
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Simon Says Game
+Welcome to the Simon Says Game! This project is a fun and interactive web-based memory game inspired by the classic Simon electronic game. Built with HTML, CSS, and JavaScript, it challenges players to memorize and replicate increasingly complex sequences of colors and sounds.
+
+# How to Play:
+Press the start button to begin.
+Watch the sequence of colors and sounds.
+Repeat the sequence by clicking the buttons in the correct order.
+The game will add an additional step to the sequence each time you succeed.
+Try to get the highest score possible!
